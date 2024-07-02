632471af003c52192be0ec07909a675397cb86fe
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-07-02 16:10:19 +0300

    Create HashSet class

--- index.js
+++ index.js
@@ -138,4 +138,107 @@ class HashMap {
   }
 }
 
+class HashSet { 
+  constructor(initialCapacity = 16, loadFactor = 0.75) {
+    this.capacity = initialCapacity;
+    this.loadFactor = loadFactor;
+    this.size = 0;
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+    return hashCode % this.capacity;
+  }
+
+  set(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i] === key) {
+        return; // do nothing if key already exists
+      }
+    }
+
+    bucket.push(key);
+    this.size++;
+
+    if (this.size / this.capacity > this.loadFactor) {
+      this.resize();
+    }
+  }
+
+  has(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+
+    return bucket.includes(key);
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    const indexToRemove = bucket.indexOf(key);
+
+    if (indexToRemove !== -1) {
+      bucket.splice(indexToRemove, 1);
+      this.size--;
+      return true;
+    }
+    return false;
+  }
+
+  length() {
+    return this.size;
+  }
+
+  clear() {
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+    this.size = 0;
+  }
+
+  keys() {
+    let result = [];
+    for (let i = 0; i < this.buckets.length; i++) {
+      for (let j = 0; j < this.buckets[i].length; j++) {
+        result.push(this.buckets[i][j]);
+      }
+    }
+    return result;
+  }
+
+  entries() {
+    let result = [];
+    for (let i = 0; i < this.buckets.length; i++) {
+      for (let j = 0; j < this.buckets[i].length; j++) {
+        result.push(this.buckets[i][j]);
+      }
+    }
+    return result.map(key => [key, undefined]);
+  }
+
+  resize() {
+    const newCapacity = this.capacity * 2;
+    const newBuckets = new Array(newCapacity).fill(null).map(() => []);
+
+    this.buckets.forEach(bucket => {
+      bucket.forEach(key => {
+        const index = this.hash(key) % newCapacity;
+        newBuckets[index].push(key);
+      });
+    });
+
+    this.capacity = newCapacity;
+    this.buckets = newBuckets;
+  }
+}
+
+/*HashSet may be useful in scenarios where you need to track the existence or uniqueness
+ of items without the need for additional data associated with each key.*/
+
 module.exports = HashMap;
